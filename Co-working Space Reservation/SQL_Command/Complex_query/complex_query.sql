da0bb6fe6c223dbbd4bab888ac1044fa0eb2d847
watthanakorn Rakthong <watthanakorn.r@gmail.com>
2025-02-03 18:31:17 +0700

    Add Complex query

--- Co-working Space Reservation/SQL_Command/Complex_query/complex_query.sql
+++ Co-working Space Reservation/SQL_Command/Complex_query/complex_query.sql
@@ -4,7 +4,7 @@ FROM (
     SELECT C."coworking_id", C."name", C."address", COUNT(RS."reservation_id") AS total_reservations,
            DENSE_RANK() OVER (ORDER BY COUNT(RS."reservation_id") DESC) AS rank
     FROM "COWORKING_SPACE" C
-    JOIN "ROOM" R ON C."coworking_id" = R."coworking_id"
+    JOIN "ROOM" R ON C.coworking_id = R.coworkingid
     JOIN "RESERVATION" RS ON R."room_id" = RS."room_id"
     WHERE RS."start_time" >= '2025-02-05 14:00:00'
           AND RS."start_time" <= '2025-03-05 14:00:00'
